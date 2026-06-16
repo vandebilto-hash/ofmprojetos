f1512adebc38693b78ce854241fe4746ac1f17b4
vandebilto junior <vandebilto.junior@ofm.corp>
2026-06-16 15:07:13 -0300

    fix: remover template do Java (API MPXJ instavel), gerar MSPDI XML no Next.js

--- apps/web/src/app/api/templates/import-project/route.ts
+++ apps/web/src/app/api/templates/import-project/route.ts
@@ -1,34 +1,190 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const serviceUrl = process.env.MPP_SERVICE_URL;
+  const xml = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Project xmlns="http://schemas.microsoft.com/project/2003/mspdi" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <UID>1</UID>
+  <Name>Template Projete-se</Name>
+  <Title>Template de Importacao - Projete-se</Title>
+  <CreationDate>2026-06-16T10:00:00</CreationDate>
+  <LastSaved>2026-06-16T10:00:00</LastSaved>
+  <ScheduleFromStart>1</ScheduleFromStart>
+  <StartDate>2026-07-01T08:00:00</StartDate>
+  <FinishDate>2026-09-10T17:00:00</FinishDate>
+  <DaysPerMonth>22</DaysPerMonth>
+  <DefaultStartTime>08:00:00</DefaultStartTime>
+  <DefaultEndTime>17:00:00</DefaultEndTime>
+  <MinutesPerDay>480</MinutesPerDay>
+  <MinutesPerWeek>2400</MinutesPerWeek>
+  <CurrencySymbol>R$</CurrencySymbol>
+  <Calendars>
+    <Calendar>
+      <UID>1</UID>
+      <Name>Padrao</Name>
+      <IsBaseCalendar>0</IsBaseCalendar>
+      <BaseCalendarUID>-1</BaseCalendarUID>
+      <WeekDays>
+        <WeekDay><DayType>1</DayType><DayWorking>0</DayWorking></WeekDay>
+        <WeekDay><DayType>2</DayType><DayWorking>1</DayWorking><WorkingTimes><WorkingTime><FromTime>08:00:00</FromTime><ToTime>12:00:00</ToTime></WorkingTime><WorkingTime><FromTime>13:00:00</FromTime><ToTime>17:00:00</ToTime></WorkingTime></WorkingTimes></WeekDay>
+        <WeekDay><DayType>3</DayType><DayWorking>1</DayWorking><WorkingTimes><WorkingTime><FromTime>08:00:00</FromTime><ToTime>12:00:00</ToTime></WorkingTime><WorkingTime><FromTime>13:00:00</FromTime><ToTime>17:00:00</ToTime></WorkingTime></WorkingTimes></WeekDay>
+        <WeekDay><DayType>4</DayType><DayWorking>1</DayWorking><WorkingTimes><WorkingTime><FromTime>08:00:00</FromTime><ToTime>12:00:00</ToTime></WorkingTime><WorkingTime><FromTime>13:00:00</FromTime><ToTime>17:00:00</ToTime></WorkingTime></WorkingTimes></WeekDay>
+        <WeekDay><DayType>5</DayType><DayWorking>1</DayWorking><WorkingTimes><WorkingTime><FromTime>08:00:00</FromTime><ToTime>12:00:00</ToTime></WorkingTime><WorkingTime><FromTime>13:00:00</FromTime><ToTime>17:00:00</ToTime></WorkingTime></WorkingTimes></WeekDay>
+        <WeekDay><DayType>6</DayType><DayWorking>1</DayWorking><WorkingTimes><WorkingTime><FromTime>08:00:00</FromTime><ToTime>12:00:00</ToTime></WorkingTime></WorkingTimes></WeekDay>
+        <WeekDay><DayType>7</DayType><DayWorking>0</DayWorking></WeekDay>
+      </WeekDays>
+    </Calendar>
+  </Calendars>
+  <Tasks>
+    ${generateTasks()}
+  </Tasks>
+  <Resources>
+    ${generateResources()}
+  </Resources>
+  <Assignments />
+</Project>`;
 
-  if (!serviceUrl) {
-    return NextResponse.json(
-      { error: "Servico MPP nao esta disponivel. O template requer o servico MPXJ." },
-      { status: 503 }
-    );
-  }
-
-  try {
-    const response = await fetch(`${serviceUrl}/template`);
-
-    if (!response.ok) {
-      return NextResponse.json({ error: "Falha ao gerar template." }, { status: 502 });
+  return new NextResponse(xml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Content-Disposition": 'attachment; filename="template-projete-se.xml"'
     }
+  });
+}
 
-    const buffer = await response.arrayBuffer();
+function generateTasks() {
+  const tasks = [
+    { uid: 1, id: 1, name: "Fase de Planejamento", level: 1, wbs: "1", start: "2026-07-01T08:00:00", finish: "2026-07-15T17:00:00", dur: "PT320H0M0S", pred: "" },
+    { uid: 2, id: 2, name: "Definicao de Escopo", level: 2, wbs: "1.1", start: "2026-07-01T08:00:00", finish: "2026-07-05T17:00:00", dur: "PT40H0M0S", pred: "" },
+    { uid: 3, id: 3, name: "Levantamento de Requisitos", level: 3, wbs: "1.1.1", start: "2026-07-01T08:00:00", finish: "2026-07-03T17:00:00", dur: "PT16H0M0S", pred: "" },
+    { uid: 4, id: 4, name: "Documentacao de Escopo", level: 3, wbs: "1.1.2", start: "2026-07-03T08:00:00", finish: "2026-07-05T17:00:00", dur: "PT16H0M0S", pred: "3FS" },
+    { uid: 5, id: 5, name: "Planejamento de Recursos", level: 2, wbs: "1.2", start: "2026-07-05T08:00:00", finish: "2026-07-10T17:00:00", dur: "PT40H0M0S", pred: "4FS" },
+    { uid: 6, id: 6, name: "Alocacao de Equipe", level: 3, wbs: "1.2.1", start: "2026-07-05T08:00:00", finish: "2026-07-08T17:00:00", dur: "PT24H0M0S", pred: "" },
+    { uid: 7, id: 7, name: "Aquisicao de Ferramentas", level: 3, wbs: "1.2.2", start: "2026-07-08T08:00:00", finish: "2026-07-10T17:00:00", dur: "PT16H0M0S", pred: "6FS" },
+    { uid: 8, id: 8, name: "Aprovacao do Planejamento", level: 2, wbs: "1.3", start: "2026-07-10T08:00:00", finish: "2026-07-15T17:00:00", dur: "PT32H0M0S", pred: "7FS" },
+    { uid: 9, id: 9, name: "Fase de Desenvolvimento", level: 1, wbs: "2", start: "2026-07-15T08:00:00", finish: "2026-08-30T17:00:00", dur: "PT400H0M0S", pred: "8FS" },
+    { uid: 10, id: 10, name: "Modulo Backend", level: 2, wbs: "2.1", start: "2026-07-15T08:00:00", finish: "2026-08-10T17:00:00", dur: "PT160H0M0S", pred: "" },
+    { uid: 11, id: 11, name: "API REST", level: 3, wbs: "2.1.1", start: "2026-07-15T08:00:00", finish: "2026-07-25T17:00:00", dur: "PT80H0M0S", pred: "" },
+    { uid: 12, id: 12, name: "Banco de Dados", level: 3, wbs: "2.1.2", start: "2026-07-25T08:00:00", finish: "2026-08-05T17:00:00", dur: "PT80H0M0S", pred: "11FS" },
+    { uid: 13, id: 13, name: "Integracao", level: 3, wbs: "2.1.3", start: "2026-08-05T08:00:00", finish: "2026-08-10T17:00:00", dur: "PT40H0M0S", pred: "12FS" },
+    { uid: 14, id: 14, name: "Modulo Frontend", level: 2, wbs: "2.2", start: "2026-07-20T08:00:00", finish: "2026-08-20T17:00:00", dur: "PT200H0M0S", pred: "" },
+    { uid: 15, id: 15, name: "Design de Interface", level: 3, wbs: "2.2.1", start: "2026-07-20T08:00:00", finish: "2026-07-30T17:00:00", dur: "PT80H0M0S", pred: "" },
+    { uid: 16, id: 16, name: "Implementacao de Componentes", level: 3, wbs: "2.2.2", start: "2026-07-30T08:00:00", finish: "2026-08-15T17:00:00", dur: "PT120H0M0S", pred: "15FS" },
+    { uid: 17, id: 17, name: "Testes de Interface", level: 3, wbs: "2.2.3", start: "2026-08-15T08:00:00", finish: "2026-08-20T17:00:00", dur: "PT40H0M0S", pred: "16FS" },
+    { uid: 18, id: 18, name: "Testes Integrados", level: 2, wbs: "2.3", start: "2026-08-20T08:00:00", finish: "2026-08-30T17:00:00", dur: "PT80H0M0S", pred: "13FS;17FS" },
+    { uid: 19, id: 19, name: "Fase de Entrega", level: 1, wbs: "3", start: "2026-08-30T08:00:00", finish: "2026-09-10T17:00:00", dur: "PT80H0M0S", pred: "18FS" },
+    { uid: 20, id: 20, name: "Deploy em Producao", level: 2, wbs: "3.1", start: "2026-08-30T08:00:00", finish: "2026-09-05T17:00:00", dur: "PT40H0M0S", pred: "" },
+    { uid: 21, id: 21, name: "Documentacao Final", level: 2, wbs: "3.2", start: "2026-09-01T08:00:00", finish: "2026-09-08T17:00:00", dur: "PT48H0M0S", pred: "" },
+    { uid: 22, id: 22, name: "Treinamento do Cliente", level: 2, wbs: "3.3", start: "2026-09-05T08:00:00", finish: "2026-09-10T17:00:00", dur: "PT40H0M0S", pred: "20FS;21FS" },
+  ];
 
-    return new NextResponse(buffer, {
-      headers: {
-        "Content-Type": "application/xml; charset=utf-8",
-        "Content-Disposition": 'attachment; filename="template-projete-se.xml"'
-      }
-    });
-  } catch {
-    return NextResponse.json(
-      { error: "Servico MPP nao esta acessivel." },
-      { status: 503 }
-    );
-  }
+  return tasks.map(t => {
+    const predXml = t.pred ? `
+        <PredecessorLinks>
+          ${t.pred.split(";").map(p => {
+            const uid = parseInt(p);
+            return `<PredecessorLink><PredecessorUID>${uid}</PredecessorUID><Type>1</Type><Lag>0</Lag><CrossProject>0</CrossProject><ExternalTask>0</ExternalTask></PredecessorLink>`;
+          }).join("\n          ")}
+        </PredecessorLinks>` : "";
+
+    return `      <Task>
+        <UID>${t.uid}</UID>
+        <ID>${t.id}</ID>
+        <Name>${t.name}</Name>
+        <Type>1</Type>
+        <IsNull>0</IsNull>
+        <CreateDate>2026-06-16T10:00:00</CreateDate>
+        <Start>${t.start}</Start>
+        <Finish>${t.finish}</Finish>
+        <Duration>${t.dur}</Duration>
+        <DurationFormat>7</DurationFormat>
+        <OutlineLevel>${t.level}</OutlineLevel>
+        <OutlineNumber>${t.wbs}</OutlineNumber>
+        <Priority>500</Priority>
+        <PercentComplete>0</PercentComplete>
+        <PercentWorkComplete>0</PercentWorkComplete>
+        <FixedCost>0</FixedCost>
+        <FixedCostAccrual>2</FixedCostAccrual>
+        <Work>PT0H0M0S</Work>
+        <ConstraintType>0</ConstraintType>
+        <CalendarUID>-1</CalendarUID>
+        <Fixed>0</Fixed>
+        <LevelingDelay>0</LevelingDelay>
+        <LevelingDelayFormat>7</LevelingDelayFormat>
+        <IgnoreResourceCalendar>0</IgnoreResourceCalendar>
+        <HideBar>0</HideBar>
+        <Rollup>0</Rollup>
+        <BCWS>0</BCWS>
+        <BCWP>0</BCWP>
+        <ACWP>0</ACWP>
+        <IsCritical>0</IsCritical>
+        <IsEffortDriven>0</IsEffortDriven>
+        <IsLocked>0</IsLocked>
+        <IsExternal>0</IsExternal>
+        <IsManual>0</IsManual>
+        <SubprojectName />
+        <SubprojectReadOnly>0</SubprojectReadOnly>
+        <ExternalTaskProject />
+        <EarlyStart>${t.start}</EarlyStart>
+        <EarlyFinish>${t.finish}</EarlyFinish>
+        <LateStart>${t.start}</LateStart>
+        <LateFinish>${t.finish}</LateFinish>
+        <StartVariance>0</StartVariance>
+        <FinishVariance>0</FinishVariance>
+        <FreeSlack>0</FreeSlack>
+        <TotalSlack>0</TotalSlack>
+        <Cost>0</Cost>
+        <OvertimeCost>0</OvertimeCost>
+        <OvertimeWork>PT0H0M0S</OvertimeWork>
+        <ActualCost>0</ActualCost>
+        <ActualWork>PT0H0M0S</ActualWork>
+        <ActualDuration>PT0H0M0S</ActualDuration>
+        <ActualStart>1984-01-01T00:00:00</ActualStart>
+        <ActualFinish>1984-01-01T00:00:00</ActualFinish>
+        <RegularWork>PT0H0M0S</RegularWork>
+        <RemainingCost>0</RemainingCost>
+        <RemainingWork>PT0H0M0S</RemainingWork>
+        <RemainingDuration>${t.dur}</RemainingDuration>
+        <Notes>Template - edite com observacoes da tarefa</Notes>
+        <Stop>1984-01-01T00:00:00</Stop>
+        <Resume>1984-01-01T00:00:00</Resume>
+        <RecalcFlags>0</RecalcFlags>${predXml}
+        <Hyperlink />
+        <HyperlinkAddress />
+        <HyperlinkSubAddress />
+        <IsRecurring>0</IsRecurring>
+        <IgnoreWarnings>0</IgnoreWarnings>
+        <IsMarked>0</IsMarked>
+      </Task>`;
+  }).join("\n");
+}
+
+function generateResources() {
+  const resources = [
+    { uid: 1, name: "Joao Silva" },
+    { uid: 2, name: "Maria Santos" },
+    { uid: 3, name: "Carlos Oliveira" },
+    { uid: 4, name: "Ana Rodrigues" },
+    { uid: 5, name: "Pedro Costa" },
+  ];
+
+  return resources.map(r => `      <Resource>
+        <UID>${r.uid}</UID>
+        <ID>${r.uid}</ID>
+        <Type>1</Type>
+        <IsNull>0</IsNull>
+        <CreateDate>2026-06-16T10:00:00</CreateDate>
+        <Name>${r.name}</Name>
+        <Cost>0</Cost>
+        <OvertimeCost>0</OvertimeCost>
+        <RegularCost>0</RegularCost>
+        <ActualCost>0</ActualCost>
+        <ActualOvertimeCost>0</ActualOvertimeCost>
+        <ActualWork>PT0H0M0S</ActualWork>
+        <ActualOvertimeWork>PT0H0M0S</ActualOvertimeWork>
+        <BaselineCost>0</BaselineCost>
+        <BaselineWork>PT0H0M0S</BaselineWork>
+        <PeakUnits>0</PeakUnits>
+        <TotalFixedCost>0</TotalFixedCost>
+        <MaxUnits>1</MaxUnits>
+      </Resource>`).join("\n");
 }
--- services/mpp-service/src/main/java/com/projetese/mpp/MppController.java
+++ services/mpp-service/src/main/java/com/projetese/mpp/MppController.java
@@ -1,23 +1,15 @@
 package com.projetese.mpp;
 
 import java.io.File;
-import java.io.OutputStream;
 import java.nio.file.Files;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Objects;
 
 import net.sf.mpxj.ProjectFile;
 import net.sf.mpxj.Task;
-import net.sf.mpxj.ProjectCalendar;
-import net.sf.mpxj.Day;
-import net.sf.mpxj.Priority;
-import net.sf.mpxj.RelationType;
-import net.sf.mpxj.mspdi.MSPDIWriter;
 import net.sf.mpxj.reader.UniversalProjectReader;
 import org.springframework.http.MediaType;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,133 +21,6 @@ public class MppController {
   @GetMapping("/health")
   public Health health() {
     return new Health("ok");
-  }
-
-  @GetMapping(value = "/template", produces = "application/octet-stream")
-  public void downloadTemplate(org.springframework.http.HttpServletResponse response) throws Exception {
-    response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"template-projete-se.xml\"");
-
-    ProjectFile project = new ProjectFile();
-
-    ProjectCalendar cal = project.getCalendar();
-    cal.setName("Padrao");
-    for (int d = 2; d <= 6; d++) {
-      Day day = Day.getValue(d);
-      cal.setWorking(day, true);
-      cal.addDefaultWorkingHours(day);
-    }
-    cal.setWorking(Day.SUNDAY, false);
-    cal.setWorking(Day.SATURDAY, true);
-    cal.addDefaultWorkingHours(Day.SATURDAY);
-
-    project.getProjectProperties().setTitle("Template Projete-se");
-    project.getProjectProperties().setStart(LocalDateTime.of(2026, 7, 1, 8, 0));
-
-    Task phase1 = createTask(project, 1, "Fase de Planejamento", 0, "1",
-      LocalDateTime.of(2026, 7, 1, 8, 0), LocalDateTime.of(2026, 7, 15, 17, 0));
-    Task t11 = createTask(project, 2, "Definicao de Escopo", 1, "1.1",
-      LocalDateTime.of(2026, 7, 1, 8, 0), LocalDateTime.of(2026, 7, 5, 17, 0));
-    Task t111 = createTask(project, 3, "Levantamento de Requisitos", 2, "1.1.1",
-      LocalDateTime.of(2026, 7, 1, 8, 0), LocalDateTime.of(2026, 7, 3, 17, 0));
-    Task t112 = createTask(project, 4, "Documentacao de Escopo", 2, "1.1.2",
-      LocalDateTime.of(2026, 7, 3, 8, 0), LocalDateTime.of(2026, 7, 5, 17, 0));
-    Task t12 = createTask(project, 5, "Planejamento de Recursos", 1, "1.2",
-      LocalDateTime.of(2026, 7, 5, 8, 0), LocalDateTime.of(2026, 7, 10, 17, 0));
-    Task t121 = createTask(project, 6, "Alocacao de Equipe", 2, "1.2.1",
-      LocalDateTime.of(2026, 7, 5, 8, 0), LocalDateTime.of(2026, 7, 8, 17, 0));
-    Task t122 = createTask(project, 7, "Aquisicao de Ferramentas", 2, "1.2.2",
-      LocalDateTime.of(2026, 7, 8, 8, 0), LocalDateTime.of(2026, 7, 10, 17, 0));
-    Task t13 = createTask(project, 8, "Aprovacao do Planejamento", 1, "1.3",
-      LocalDateTime.of(2026, 7, 10, 8, 0), LocalDateTime.of(2026, 7, 15, 17, 0));
-
-    Task phase2 = createTask(project, 9, "Fase de Desenvolvimento", 0, "2",
-      LocalDateTime.of(2026, 7, 15, 8, 0), LocalDateTime.of(2026, 8, 30, 17, 0));
-    Task t21 = createTask(project, 10, "Modulo Backend", 1, "2.1",
-      LocalDateTime.of(2026, 7, 15, 8, 0), LocalDateTime.of(2026, 8, 10, 17, 0));
-    Task t211 = createTask(project, 11, "API REST", 2, "2.1.1",
-      LocalDateTime.of(2026, 7, 15, 8, 0), LocalDateTime.of(2026, 7, 25, 17, 0));
-    Task t212 = createTask(project, 12, "Banco de Dados", 2, "2.1.2",
-      LocalDateTime.of(2026, 7, 25, 8, 0), LocalDateTime.of(2026, 8, 5, 17, 0));
-    Task t213 = createTask(project, 13, "Integracao", 2, "2.1.3",
-      LocalDateTime.of(2026, 8, 5, 8, 0), LocalDateTime.of(2026, 8, 10, 17, 0));
-    Task t22 = createTask(project, 14, "Modulo Frontend", 1, "2.2",
-      LocalDateTime.of(2026, 7, 20, 8, 0), LocalDateTime.of(2026, 8, 20, 17, 0));
-    Task t221 = createTask(project, 15, "Design de Interface", 2, "2.2.1",
-      LocalDateTime.of(2026, 7, 20, 8, 0), LocalDateTime.of(2026, 7, 30, 17, 0));
-    Task t222 = createTask(project, 16, "Implementacao de Componentes", 2, "2.2.2",
-      LocalDateTime.of(2026, 7, 30, 8, 0), LocalDateTime.of(2026, 8, 15, 17, 0));
-    Task t223 = createTask(project, 17, "Testes de Interface", 2, "2.2.3",
-      LocalDateTime.of(2026, 8, 15, 8, 0), LocalDateTime.of(2026, 8, 20, 17, 0));
-    Task t23 = createTask(project, 18, "Testes Integrados", 1, "2.3",
-      LocalDateTime.of(2026, 8, 20, 8, 0), LocalDateTime.of(2026, 8, 30, 17, 0));
-
-    Task phase3 = createTask(project, 19, "Fase de Entrega", 0, "3",
-      LocalDateTime.of(2026, 8, 30, 8, 0), LocalDateTime.of(2026, 9, 10, 17, 0));
-    Task t31 = createTask(project, 20, "Deploy em Producao", 1, "3.1",
-      LocalDateTime.of(2026, 8, 30, 8, 0), LocalDateTime.of(2026, 9, 5, 17, 0));
-    Task t32 = createTask(project, 21, "Documentacao Final", 1, "3.2",
-      LocalDateTime.of(2026, 9, 1, 8, 0), LocalDateTime.of(2026, 9, 8, 17, 0));
-    Task t33 = createTask(project, 22, "Treinamento do Cliente", 1, "3.3",
-      LocalDateTime.of(2026, 9, 5, 8, 0), LocalDateTime.of(2026, 9, 10, 17, 0));
-
-    addLink(t112, t111, RelationType.FINISH_START);
-    addLink(t12, t112, RelationType.FINISH_START);
-    addLink(t122, t121, RelationType.FINISH_START);
-    addLink(t13, t122, RelationType.FINISH_START);
-    addLink(phase2, t13, RelationType.FINISH_START);
-    addLink(t212, t211, RelationType.FINISH_START);
-    addLink(t213, t212, RelationType.FINISH_START);
-    addLink(t222, t221, RelationType.FINISH_START);
-    addLink(t223, t222, RelationType.FINISH_START);
-    addLink(t23, t213, RelationType.FINISH_START);
-    addLink(t23, t223, RelationType.FINISH_START);
-    addLink(phase3, t23, RelationType.FINISH_START);
-    addLink(t33, t31, RelationType.FINISH_START);
-    addLink(t33, t32, RelationType.FINISH_START);
-
-    phase1.addChildTask(t11);
-    phase1.addChildTask(t12);
-    phase1.addChildTask(t13);
-    t11.addChildTask(t111);
-    t11.addChildTask(t112);
-    t12.addChildTask(t121);
-    t12.addChildTask(t122);
-    phase2.addChildTask(t21);
-    phase2.addChildTask(t22);
-    phase2.addChildTask(t23);
-    t21.addChildTask(t211);
-    t21.addChildTask(t212);
-    t21.addChildTask(t213);
-    t22.addChildTask(t221);
-    t22.addChildTask(t222);
-    t22.addChildTask(t223);
-    phase3.addChildTask(t31);
-    phase3.addChildTask(t32);
-    phase3.addChildTask(t33);
-
-    MSPDIWriter writer = new MSPDIWriter();
-    writer.write(project, response.getOutputStream());
-    response.getOutputStream().flush();
-  }
-
-  private Task createTask(ProjectFile project, int uid, String name, int outlineLevel, String outlineNumber,
-      LocalDateTime start, LocalDateTime finish) {
-    Task task = project.addTask();
-    task.setUniqueID(uid);
-    task.setID(uid);
-    task.setName(name);
-    task.setOutlineLevel(outlineLevel + 1);
-    task.setOutlineNumber(outlineNumber);
-    task.setStart(start);
-    task.setFinish(finish);
-    task.setPercentageComplete(0.0);
-    task.setPriority(Priority.getInstance(500));
-    task.setNotes("Template de importacao - edite este campo com observacoes da tarefa.");
-    return task;
-  }
-
-  private void addLink(Task successor, Task predecessor, RelationType type) {
-    successor.addPredecessor().predecessorTask(predecessor).type(type).finish();
   }
 
   @PostMapping(path = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
